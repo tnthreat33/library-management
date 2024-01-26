a75cc1b95fb0554276c0eb204ece8922ff4bd9f9
Rachel Hamby <tnthreat33@gmail.com>
2024-01-26 13:45:51 -0500

    better error messages for create transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,27 +1,30 @@
 class TransactionsController < ApplicationController
 
-    def create
-        user = User.find(params[:user_id])
-        book = Book.find(params[:book_id])
+  def create
+    user = User.find(params[:user_id])
+    book = Book.find(params[:book_id])
+  
+    if !user.can_check_out_book
+      render json: { error: "Sorry, you can't check out the book. You have reached the limit of checked-out books or have overdue books." }, status: :unprocessable_entity
+    elsif book.available_copies <= 0
+      render json: { error: "Sorry, no more available copies of this book. Please check back later." }, status: :unprocessable_entity
+    else
+      transaction = Transaction.create(
+        user: user,
+        book: book,
+        transaction_type: Transaction.transaction_types[:check_out],
+        transaction_date: Date.today,
+        due_date: 2.weeks.from_now
+      )
+  
+      book.update(available_copies: book.available_copies - 1)
+  
+      render json: transaction
+    end
+  end
     
-        if user.can_check_out_book && book.available_copies.positive?
-            transaction = user.transactions.create(
-            book: book,
-            transaction_type: Transaction.transaction_types[:check_out],
-            transaction_date: Date.today,
-            due_date: 2.weeks.from_now
-          )
-    
-          book.update(available_copies: book.available_copies - 1)
-    
-          render json: transaction
-        else
-          render json: { error: "Invalid request for check-out." }, status: :unprocessable_entity
-        end
-      end
-    
-      def update
-        transaction = Transaction.find_by(id: params[:id])
+    def update
+      transaction = Transaction.find_by(id: params[:id])
     
         if transaction.present?
           if transaction.return?
@@ -35,6 +38,6 @@ class TransactionsController < ApplicationController
         else
           render json: { error: "Invalid transaction or book is not checked out by the user." }, status: :unprocessable_entity
         end
-      end
+    end
     
 end
