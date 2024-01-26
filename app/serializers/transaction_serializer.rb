bf3137a39b49170ef2be301c184eeadf75599821
Rachel Hamby <tnthreat33@gmail.com>
2024-01-26 16:10:20 -0500

    serializer associations

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :book_id, :transaction_type, :transaction_date, :due_date
 
-belongs_to :book
+  belongs_to :book
 end
