eb3262bae62e12f8ce4dd390d02b27b0c59b084d
Rachel Hamby <tnthreat33@gmail.com>
2024-01-25 20:57:09 -0500

    book transaction validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,10 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+  validates :transaction_date, presence: true
+  validates :transaction_type, presence: true
+  validates :due_date, presence: true
+    
+
   enum transaction_type: { check_out: 0, return: 1 }
 end
