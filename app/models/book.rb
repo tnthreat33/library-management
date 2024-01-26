620a46df5ddc861bbc1bf6b2ce9842af1afdfe4b
Rachel Hamby <tnthreat33@gmail.com>
2024-01-25 20:55:03 -0500

    book validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,11 @@
 class Book < ApplicationRecord
     has_many :transactions
 
+    validates :ISBN, presence: true
+    validates :total_copies, :available_copies, numericality: { greater_than_or_equal_to: 0 }
+    validates :title, presence: true
+    validates :author, presence: true
+
     def self.add_or_update_book(book_params)
         existing_book = find_by(ISBN: book_params[:ISBN])
     
